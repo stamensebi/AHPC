2256e5abcf648717dd915230b3a66898343b6a8a
Stamen Sebastian <ss15807@my.bristol.ac.uk>
2018-05-10 21:03:48 +0100

    re

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -676,7 +676,7 @@ float calc_reynolds(const t_param params, t_speed* cells, int* obstacles)
 {
   const float viscosity = 1.f / 6.f * (2.f / params.omega - 1.f);
   float* idx[2];
-  return av_velocity(params, cells, obstacles, 0, 10, ) * params.reynolds_dim / viscosity;
+  return av_velocity(params, cells, obstacles, 0, 10, idx) * params.reynolds_dim / viscosity;
 }
 
 float total_density(const t_param params, t_speed* cells)
