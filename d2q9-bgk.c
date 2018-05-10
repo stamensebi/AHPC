8ed19dc05d1e233f5f9e22cb8405a8ebe828e690
Stamen Sebastian <ss15807@my.bristol.ac.uk>
2018-05-10 15:38:12 +0100

    re

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -193,8 +193,6 @@ int main(int argc, char* argv[])
   //   block_h++;
 
   //Initialise MPI buffers
-  int bufSize = sizeof(t_speed) * params.nx;
-
 
   sendbuf = (t_speed*)malloc(sizeof(t_speed) * params.nx);
   recvbuf = (t_speed*)malloc(sizeof(t_speed) * params.nx);
@@ -217,10 +215,10 @@ int main(int argc, char* argv[])
            recvbuf, params.nx, Cell, down, tag, MPI_COMM_WORLD, &status);
 
 
-    printf("RECEIVED: " );
-    //for (int i = 0; i < params.nx; i++)
-      printf("%.6f\n", recvbuf[0].speeds[0]);
-      printf("SENT:   %.6f ", sendbuf[0].speeds[0] );
+     printf("%d RECEIVED: ", myrank );
+    // //for (int i = 0; i < params.nx; i++)
+       printf("%.6f FROM %d\n", recvbuf[0].speeds[0], down);
+       printf("SENT:   %.6f ", sendbuf[0].speeds[0] );
 
     timestep(params, cells, tmp_cells, obstacles);
     av_vels[tt] = av_velocity(params, cells, obstacles);
